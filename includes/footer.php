2c27af4d54957dc6acecc5145fbfeb6db4a8bc2a
Lucas Faustino <pimbacacete2016@gmail.com>
2026-07-14 13:23:17 +0100

    s

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,6 @@
     <div class="footer-container">
         <p>&copy; <?php echo date('Y'); ?> Agrupamento de Escolas Canelas - Sistema de Reservas</p>
         <p class="footer-small">Desenvolvido como Prova de Aptidão Profissional</p>
+        <p class="footer-small">Desenvolvido como Prova de Aptidão Profissional</p>
     </div>
 </footer>
